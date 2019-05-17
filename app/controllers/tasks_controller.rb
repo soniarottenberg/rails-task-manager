aa9edf82f54d19b0b545e5f4e6611e872e4ce489
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2019-05-17 17:14:21 +0200

    failed attempt at refacto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  # before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -33,6 +35,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  # def set_task
+  #   @task = Task.find(params[:id])
+  # end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'tasks', to: 'tasks#index', as: :tasks
-
-get 'tasks/new', to: 'tasks#new'
-
-patch 'tasks/:id', to: 'tasks#update'
-
-get 'tasks/:id/edit', to: 'tasks#edit'
-
-get 'tasks/:id', to: 'tasks#show', as: :task
-
-post 'tasks', to:'tasks#create'
-
-delete 'tasks/:id', to: 'tasks#destroy'
-# resources :tasks
+# get 'tasks', to: 'tasks#index', as: :tasks
+#
+# get 'tasks/new', to: 'tasks#new'
+#
+# patch 'tasks/:id', to: 'tasks#update'
+#
+# get 'tasks/:id/edit', to: 'tasks#edit'
+#
+# get 'tasks/:id', to: 'tasks#show', as: :task
+#
+# post 'tasks', to:'tasks#create'
+#
+# delete 'tasks/:id', to: 'tasks#destroy'
+resources :tasks
 end
